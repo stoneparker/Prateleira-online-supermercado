1939364d347e9a4c647288bb389661ed95915287
unknown <heyvitoria.lopes@gmail.com>
2020-03-19 22:07:02 -0300

    Menu de setores adicionado (sem identificação de click)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,56 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, FlatList, StyleSheet} from 'react-native'
 
 export default function HomeScreen(){
+   const sectors = {
+      data: [
+         "Tudo",
+         "Mercearia",
+         "Frios e Laticínios",
+         "Adega",
+         "Higiene e Limpeza",
+         "Hortifruti",
+      ]
+   }
+
    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-         <Text>Mercado...</Text>
+      <View style={{ flex: 1, alignItems: 'center' }}>
+         <View style={styles.menu}>
+            <FlatList 
+               style={styles.flatlist}
+               data={sectors.data}
+               renderItem={({ item }) => <Text style={styles.sector}>{item}</Text>}
+               keyExtractor={item => item}
+               horizontal
+               showsHorizontalScrollIndicator={false}
+            />
+         </View>
       </View>
    );
 }
 
-
+const styles = StyleSheet.create({
+   menu: {
+      height: '8%',
+      width: '100%',
+      backgroundColor: '#9E0000',
+      top: 0
+   },
+   flatlist: {
+      height: '100%',
+   },
+   sector: {
+      backgroundColor: '#870000',
+      height: '100%',
+      marginLeft: 5,
+      marginRight: 5,
+      paddingLeft: 10,
+      paddingRight: 10,
+      textAlignVertical: "center",
+      color: '#FFF',
+      fontSize: 16,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+ }
+})
 
